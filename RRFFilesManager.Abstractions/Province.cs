6dee7dc9c82b94e41b9cabd2410136e3cd40f774
fcoa <fcoa@ivolutia.com>
2020-10-11 17:40:01 -0300

    added new projects

---
+++ RRFFilesManager.Abstractions/Province.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RRFFilesManager.Abstractions
+{
+    public class Province
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RRFFilesManager.DataAccess/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using RRFFilesManager.Abstractions;
+using System;
+using System.Data.Entity;
+
+namespace RRFFilesManager.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(string nameOrConnectionString): base(nameOrConnectionString) { }
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Province> Provinces { get; set; }
+    }
+}
