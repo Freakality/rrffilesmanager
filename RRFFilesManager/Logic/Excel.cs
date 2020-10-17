62c9600ccac752f3e664677076fb92442c9b233e
fcoa <fcoa@ivolutia.com>
2020-10-17 00:51:30 -0300

    fixed bugs

---
+++ RRFFilesManager/Logic/Excel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRFFilesManager.Logic
+{
+    public class Excel
+    {
+        public static void ReplaceAll(Worksheet worksheet, string findText, string replaceWith)
+        {
+            worksheet.Cells.Replace(findText, replaceWith);
+        }
+        public static void ReplaceAll(Worksheet worksheet, string findText, bool replaceWith)
+        {
+            worksheet.Cells.Replace(findText, replaceWith ? "Yes" : "No");
+        }
+        public static void ReplaceAll(Worksheet worksheet, string findText, DateTime? replaceWith)
+        {
+            worksheet.Cells.Replace(findText, replaceWith?.ToString("MMMM d, yyyy"));
+        }
+        public static void ReplaceAll(Worksheet worksheet, string findText, int replaceWith)
+        {
+            worksheet.Cells.Replace(findText, replaceWith.ToString());
+        }
+    }
+}
