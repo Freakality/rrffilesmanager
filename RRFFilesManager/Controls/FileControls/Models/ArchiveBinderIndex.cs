b214c6db42ffbdd62d8672a5a78b5a34ac7ff769
fcoa <felixcoa@hotmail.com>
2021-04-08 09:55:01 -0300

    Correccion de estilos

--- RRFFilesManager/Controls/FileControls/Models/ArchiveBinderIndex.cs
+++ RRFFilesManager/Controls/FileControls/Models/ArchiveBinderIndex.cs
@@ -7,13 +7,25 @@ using System.Threading.Tasks;
 
 namespace RRFFilesManager.Controls.FileControls.Models
 {
-    public class ArchiveBinderIndex : ArchiveBinder
+    public class ArchiveBinderIndex
     {
-
-        public ArchiveBinderIndex(Archive archive) : base(archive)
+        private Archive archive;
+        public ArchiveBinderIndex(Archive archive)
         {
+            ID = archive.ID;
+            Type = archive.DocumentType;
+            Date = archive.Created;
+            Name = archive.Name;
             IndexCategory = archive.DocumentType?.IndexCategory;
+            this.archive = archive;
         }
+        public bool Check { get; set; }
+        public int ID { get; set; }
+        public DocumentType Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
         public string IndexCategory { get; set; }
+
+        public Abstractions.Archive GetArchive() => this.archive;
     }
 }
